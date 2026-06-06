0271bcc61c467fdcfcb14f005cda41c9ff6a1f40
rudi <hello@rudi.company>
2026-06-06 14:45:00 +0200

    Initial Commit

---
+++ mott/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  mott
+//
+//  Created by Deny Tsjapanov on 06/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ mottTests/mottTests.swift
@@ -0,0 +1,19 @@
+//
+//  mottTests.swift
+//  mottTests
+//
+//  Created by Deny Tsjapanov on 06/06/2026.
+//
+
+import Testing
+@testable import mott
+
+struct mottTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
